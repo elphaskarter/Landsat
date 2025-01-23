21b41abeb83d07fa80de4c76031e65f2d1d8ce5d
elphaskarter <ek3336@g.rit.edu>
2025-01-23 05:42:17 -0500

    Initial commit

---
+++ Sentinel.py
@@ -0,0 +1,10 @@
+# Test file for importing libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import torch    
+import torch.nn as nn
+import torch.nn.functional as F
+
+rgb_file = "data/Sentinel2_L1C_RGB.tif"
